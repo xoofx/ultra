bb94e8e8492cf5bdd44817e8a11472c4ff032853
Alexandre Mutel <alexandre_mutel@live.com>
2026-07-04 18:25:57 +0200

    Fix build_native.sh

--- src/Ultra.Sampler/build_native.sh
+++ src/Ultra.Sampler/build_native.sh
@@ -2,7 +2,7 @@
 # Build script for the compiling the C# sampler to native
 dotnet publish -c Release -r osx-arm64
 LIB_NAME=libUltraSampler.dylib
-LIB_OUTPUT_PATH=./bin/Release/net8.0/osx-arm64/publish/$LIB_NAME
+LIB_OUTPUT_PATH=./bin/Release/net10.0/osx-arm64/publish/$LIB_NAME
 #install_name_tool -id @loader/$LIB_NAME $LIB_OUTPUT_PATH
 install_name_tool -id @loader_path/$LIB_NAME $LIB_OUTPUT_PATH
 cp $LIB_OUTPUT_PATH ./$LIB_NAME
